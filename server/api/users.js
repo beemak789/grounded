d662bab3ec05c80676d2c23a2b5ea13fb88a926e
Tiya Lee <Bigtiyalee@gmail.com>
2021-09-21 13:03:35 -0400

    added post

--- server/api/users.js
+++ server/api/users.js
@@ -5,9 +5,20 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
+      attributes: ['id', 'username']
+    })
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id = req.params.id
+      },
       attributes: ['id', 'username']
     })
     res.json(users)
