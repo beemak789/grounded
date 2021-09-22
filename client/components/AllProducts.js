189038f75dc3ed73ed7a00d2cfe4ac990cb76ff7
Brandy Mak <bmak789@gmail.com>
2021-09-22 15:46:53 -0400

    merged producer reducers

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,26 +1,24 @@
-import React, {useEffect}  from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {fetchProducts} from '../store/products'
+import { fetchProducts } from '../store/productsReducer';
 
 function allProducts() {
-
-  const products = useSelector((state) => state.productsReducer);
+  const products = useSelector((state) => state.allProducts);
 
   const dispatch = useDispatch();
 
-  useEffect( () => {
-    dispatch(fetchProducts())
-  }, [])
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
 
   return (
     <div>
       <h1>Shop Coffee</h1>
       {products.map((product) => (
         <p>{product.name}</p>
-
       ))}
     </div>
-  )
+  );
 }
 
 export default allProducts;
--- client/store/productsReducer.js
+++ client/store/productsReducer.js
@@ -1,6 +1,35 @@
 import axios from "axios";
 
 
+//ACTION TYPES:
+
+const PRODUCTS_REQUEST = 'PRODUCTS_REQUEST';
+
+//action creator
+export const setProducts = (products) => ({
+	type: PRODUCTS_REQUEST,
+	products,
+});
+
+//thunk creator
+export const fetchProducts = () => {
+	return async (dispatch) => {
+		const { data: products } = await axios.get('/api/products');
+		dispatch(setProducts(products));
+	};
+};
+
+//reducer
+export function productsReducer(state = [], action) {
+	switch (action.type) {
+		case PRODUCTS_REQUEST:
+			return action.products;
+		default:
+			return state;
+	}
+}
+
+
 //Function: Render/visualize single product on screen, upon clicking on a product
 //from "All Products" page
 export const SINGLE_PRODUCT_REQUEST = "SINGLE_PRODUCT_REQUEST"
