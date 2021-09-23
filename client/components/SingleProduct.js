6ce12b3a45c552936a48888d4edbf30d5537c161
Brandy Mak <bmak789@gmail.com>
2021-09-23 09:11:03 -0400

    attempted PUT/API/CART route, fixed singleProduct view code

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -8,30 +8,31 @@ import { Link } from 'react-router-dom';
 const SingleProduct = ({ history, match }) => {
   //UseState -- state should be used for increasing quantity of the product
   //The quantity?
-// const [qty, setQty] = useState(0)
+  // const [qty, setQty] = useState(0)
 
   // = mapDispatchToProps
   const dispatch = useDispatch();
 
   // = mapStateToProps
-  let singleProduct = useSelector((state) => state.singleProduct);
+  let singleProduct = useSelector((state) => state.singleProduct) || null;
 
   //same as componentDidMount
   useEffect(() => {
     dispatch(fetchSingleProduct(match.params.id));
   }, []);
 
-
   //Add Button Handler
   const addToCartHandler = () => {
     //When the "add to cart" button is clicked - our cart's total quantity should increase.
-    console.log("The Add To Cart Button was clicked!")
+    console.log('The Add To Cart Button was clicked!');
+  };
+
+  //Add To Quantity Handler
+  const addToQuantityHandler = () => {
+    console.log("Customer changed quantity of the item!")
   }
 
-  singleProduct = singleProduct || {}
-
-
-  if (Object.keys(singleProduct).length === 0) {
+  if (!singleProduct) {
     return <h1>Loading...</h1>;
   }
 
@@ -42,13 +43,11 @@ const SingleProduct = ({ history, match }) => {
       <img src={singleProduct.imageUrl}></img>
       <h2>{singleProduct.description}</h2>
       <h3>${singleProduct.price}</h3> <span>{singleProduct.stars}</span>
-      <h1>
-        {singleProduct && singleProduct.quantity > 0 ? "In Stock": "Out of Stock"}
-      </h1>
-      <button
-        onClick={addToCartHandler}
-        type="button"
-      >Add To Cart</button>
+      <h1>{singleProduct.quantity ? 'In Stock' : 'Out of Stock'}</h1>
+      <button onClick={addToCartHandler} type='button'>
+        Add To Cart
+      </button>
+    <button onClick={addToQuantityHandler} type="button">Select</button>
     </>
   );
 };
