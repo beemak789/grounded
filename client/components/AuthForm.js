5d1972544d15f7a206d3070dca2d93022b8795db
Brandy Mak <bmak789@gmail.com>
2021-11-06 00:12:44 -0400

    fixed sign in page

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,41 +1,60 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {authenticate} from '../store'
+import React from 'react';
+import { connect } from 'react-redux';
+import { authenticate } from '../store';
+import { Link } from 'react-router-dom'
 
 /**
  * COMPONENT
  */
- const AuthForm = props => {
-  const {name = "", displayName = "", handleSubmit = null, error = {}} = props
+const AuthForm = (props) => {
+  const {
+    name = '',
+    displayName = '',
+    handleSubmit = null,
+    error = {},
+  } = props;
 
   return (
-    <div className="auth-form">
-
-      <h1>Hello there </h1>
-      <p>Sign in or join Grounded for your daily dose of coffee.</p>
+    <div className='auth-form'>
+      <p className='sign-in-title'>Sign In </p>
+      <p className='sign-in-message'>
+        Sign into your Grounded Coffee member account
+      </p>
 
       <form onSubmit={handleSubmit} name={name}>
-        <div className="username-form">
-          <label htmlFor="username">
+        <div className='username-form'>
+          <label htmlFor='username'>
             <small>Username</small>
           </label>
-          <input name="username" type="text" />
+          <input name='username' type='text' />
         </div>
         <div>
-          <label htmlFor="password">
+          <label htmlFor='password'>
             <small>Password</small>
           </label>
-          <input name="password" type="password" />
+          <input name='password' type='password' />
         </div>
         <br />
         <div>
-          <button className="button3" type="submit">{displayName}</button>
+          <button className='button3' type='submit'>
+            {/* {displayName} */}
+            SIGN IN
+          </button>
         </div>
-        {/* {error && error.response && <div> {error.response.data} </div>} */}
       </form>
+      <div className="no-password-message">
+        <p>
+          Forgot Password?
+        </p>
+      </div>
+      <div className="no-account-message">
+        <p>
+          Don't have an account? <Link to='/signup'> <span style={{color: "#CD2626"}}>Sign Up </span></Link>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 /**
  * CONTAINER
  *   Note that we have two different sets of 'mapStateToProps' functions -
@@ -43,33 +62,33 @@ import {authenticate} from '../store'
  *   function, and share the same Component. This is a good example of how we
  *   can stay DRY with interfaces that are very similar to each other!
  */
-const mapLogin = state => {
+const mapLogin = (state) => {
   return {
     name: 'login',
     displayName: 'Login',
     // error: state.auth.error
-  }
-}
+  };
+};
 
-const mapSignup = state => {
+const mapSignup = (state) => {
   return {
     name: 'signup',
     displayName: 'Sign Up',
     // error: state.auth.error
-  }
-}
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleSubmit(evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const username = evt.target.username.value
-      const password = evt.target.password.value
-      dispatch(authenticate(username, password, formName))
-    }
-  }
-}
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const username = evt.target.username.value;
+      const password = evt.target.password.value;
+      dispatch(authenticate(username, password, formName));
+    },
+  };
+};
 
-export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+export const Login = connect(mapLogin, mapDispatch)(AuthForm);
+export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
--- client/components/Landing.js
+++ client/components/Landing.js
@@ -17,12 +17,14 @@ function Landing() {
         className='home-image'
         src='https://blue-bottle-cms.global.ssl.fastly.net/hbhhv9rz9/image/upload/v1632873419/guqrxpyvr0lscp4jdlor.jpg'
       ></img>
+      <div className="landing-page-buttons">
       <Link to='/products'>
         <button className='shop-now-button'>Get Started</button>{' '}
       </Link>
       <Link to='/login'>
         <button className='member-login'>A Member? Log In</button>
       </Link>
+      </div>
     </div>
   );
 }
